746b9ab20c7b6554081542063801149837756152
Emeka Nwobu <emekanwobu2018@gmail.com>
2023-06-15 08:19:16 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-/*string initialisation*/
-char Mystr[] = "with proper grammar, but the outcome is a piece of art,";
-printf(Mystr); /*writing the string to stdout*/
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
