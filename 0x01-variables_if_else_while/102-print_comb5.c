0773b14cd4842e816d1f1e52c498ff0dbc8f71c5
Emeka Nwobu <emekanwobu2018@gmail.com>
2023-06-19 08:55:54 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,34 +5,31 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0
+*/
 
 int main(void)
 {
-	int d, p, q;
+	int p, q;
 
-	for (d = '0'; d < '9'; d++)
+	for (p = 0; p <= 98; p++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			for (q = p + 1; q <= '9'; q++)
-			{
-				if ((p != d) != q)
-				{
-					putchar(d);
-					putchar(p);
-					putchar(q);
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-					if (d == '7' && p == '8')
-						continue;
+			if (p == 98 && q == 99)
+				continue;
 
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
