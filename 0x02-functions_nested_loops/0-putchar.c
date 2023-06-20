f085fa9433cc9246eeb2bb676dbcc2c1638bb28b
Emeka Nwobu <emekanwobu2018@gmail.com>
2023-06-20 06:16:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Check description
@@ -12,8 +12,8 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 8; i++)
-		putchar(word[i]);
-	putchar('\n');
+		_putchar(word[i]);
+	_putchar('\n');
 
 	return (0);
 }
