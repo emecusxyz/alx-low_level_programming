05be49e56156f1b378a7fd014a5691a9bfcf967d
Emeka Nwobu <emekanwobu2018@gmail.com>
2023-07-19 08:30:00 -0700

    appended

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
- */
-int main(int argc, char *argv[])
-{
-	int bytes, i;
-	unsigned char *func_ptr;
 
-	if (argc != 2)
+	/**
+	 * main - prints its own opcodes
+	 * @argc: number of arguments
+	 * @argv: array of arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int main(int argc, char *argv[])
 	{
-		printf("Error\n");
-		exit(1);
+		int bytes, i;
+		char *arr;
+
+
+		if (argc != 2)
+
+		{
+			printf("Error\n");
+			exit(1);
+		}
+
+
+		bytes = atoi(argv[1]);
+
+
+		if (bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+
+		arr = (char *)main;
+
+
+		for (i = 0; i < bytes; i++)
+		{
+			if (i == bytes - 1)
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
+			}
+			printf("%02hhx ", arr[i]);
+		}
+		return (0);
 	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
-	return (0);
-}
+
+
